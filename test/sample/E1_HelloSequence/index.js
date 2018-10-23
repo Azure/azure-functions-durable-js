0234b94f821616e47fea23ebb243bb9605ae7caf
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-10-24 01:02:55 +0900

    callActivityAsync is not there callActivity

--- test/sample/E1_HelloSequence/index.js
+++ test/sample/E1_HelloSequence/index.js
@@ -3,9 +3,9 @@ const df = require("durable-functions");
 module.exports = df.orchestrator(function*(context){
     context.log("Starting chain sample");
     const output = [];
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "Tokyo"));
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "Seattle"));
-    output.push(yield context.df.callActivityAsync("E1_SayHello", "London"));
+    output.push(yield context.df.callActivity("E1_SayHello", "Tokyo"));
+    output.push(yield context.df.callActivity("E1_SayHello", "Seattle"));
+    output.push(yield context.df.callActivity("E1_SayHello", "London"));
 
     return output;
 });
