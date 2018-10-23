6027cf1f129f47d230376e1c28b8536c8575f431
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-10-24 00:37:30 +0900

    fit to the current version

--- test/sample/E1_HelloSequence/index.js
+++ test/sample/E1_HelloSequence/index.js
@@ -1,6 +1,6 @@
 const df = require("durable-functions");
 
-module.exports = df(function*(context){
+module.exports = df.orchestrator(function*(context){
     context.log("Starting chain sample");
     const output = [];
     output.push(yield context.df.callActivityAsync("E1_SayHello", "Tokyo"));
