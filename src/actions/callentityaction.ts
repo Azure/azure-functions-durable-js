d52cc5cf913960a987f835e594d134da1fef543e
Connor McMahon <comcmaho@microsoft.com>
2019-10-03 13:15:09 -0700

    Fix up CallEntityAction verification

--- src/actions/callentityaction.ts
+++ src/actions/callentityaction.ts
@@ -10,7 +10,8 @@ export class CallEntityAction implements IAction {
         public readonly operation: string,
         public readonly input?: unknown,
     ) {
+        Utils.throwIfEmpty(entityId, "entityId");
+        Utils.throwIfEmpty(operation, "operation");
         this.instanceId = EntityId.getSchedulerIdFromEntityId(entityId);
-        Utils.throwIfEmpty(this.instanceId, "entity");
     }
 }
