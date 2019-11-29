303ddde5212da225d7eff75f1c4034f91dc61235
Anthony Chu <antchu@microsoft.com>
2019-11-29 13:00:49 -0800

    Change test to use strongly typed context

--- test/testobjects/TestOrchestrations.ts
+++ test/testobjects/TestOrchestrations.ts
@@ -1,4 +1,5 @@
 import * as df from "../../src";
+import { IOrchestrationFunctionContext } from "../../src/classes";
 
 export class TestOrchestrations {
     public static AnyAOrB: any = df.orchestrator(function*(context: any) {
@@ -182,7 +183,7 @@ export class TestOrchestrations {
         return output;
     });
 
-    public static SendHttpRequest: any = df.orchestrator(function*(context: any) {
+    public static SendHttpRequest: any = df.orchestrator(function*(context: IOrchestrationFunctionContext) {
         const input = context.df.getInput() as df.DurableHttpRequest;
         const output = yield context.df.callHttp(
             input.method,
