40d6847bc3d3d5395842d80f6e8749f3d9ae4bfe
Connor McMahon <comcmaho@microsoft.com>
2020-01-24 13:30:12 -0800

    Add comment describing DurableError

--- src/durableerror.ts
+++ src/durableerror.ts
@@ -1,3 +1,8 @@
+/**
+ * A specfic error thrown when a scheduled activity or suborchestrator has failed.
+ * This error can be checked for via `instanceof` guards to catch only exceptions thrown
+ * by the DurableJS library.
+ */
 export class DurableError extends Error {
     constructor(message: string | undefined) {
         super(message);
