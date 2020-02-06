45ed810eaec220972d3e732f4152c191f711dadc
Chris Gillum <cgillum@microsoft.com>
2020-02-05 17:30:17 -0800

    Update DurableClient to support local RPC (#152)
    - Also fixed linter warnings
    - Also fixed error message reporting

--- src/orchestrationfailureerror.ts
+++ src/orchestrationfailureerror.ts
@@ -6,17 +6,17 @@ const outOfProcDataLabel = "\n\n$OutOfProcData$:";
  * A wrapper for all errors thrown within an orchestrator function. This exception will embed
  * the orchestrator state in a way that the C# extension knows how to read so that it can replay the
  * actions scheduled before throwing an exception. This prevents non-determinism errors in Durable Task.
- * 
+ *
  * Note that making any changes to the following schema to OrchestrationFailureError.message could be considered a breaking change:
- * 
+ *
  * "<error message as a string>\n\n$OutOfProcData$<json representation of state>"
  */
 export class OrchestrationFailureError extends Error {
     constructor(error: any, state: OrchestratorState) {
-        let errorMessage : String;
+        let errorMessage: string;
         if (error instanceof Error) {
-            errorMessage= error.message;
-        } else if (error instanceof String) {
+            errorMessage = error.message;
+        } else if (typeof(error) === "string") {
             errorMessage = error;
         } else {
             errorMessage = JSON.stringify(error);
