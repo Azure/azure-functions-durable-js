d20aea8a900f68f6b106418c3f168cc87f5bbe40
David Justo <david.justo.1996@gmail.com>
2021-06-24 11:36:58 -0700

    Remove generics from callEntity calls, which do not have generics _yet_

--- samples/ShoppingOrchestration/index.ts
+++ samples/ShoppingOrchestration/index.ts
@@ -1,23 +1,23 @@
 import * as df from "durable-functions";
-import { CartItem, items, Operations } from "../Shopping/data";
+import { items, Operations } from "../Shopping/data";
 
 module.exports = df.orchestrator(function* (context) {
     const entityId = new df.EntityId("ShoppingCart", "shoppingCart");
 
-    yield context.df.callEntity<CartItem>(entityId, Operations.ADD_ITEM, {
+    yield context.df.callEntity(entityId, Operations.ADD_ITEM, {
         itemId: items[0].itemId,
         quantity: 1,
     });
-    yield context.df.callEntity<CartItem>(entityId, Operations.ADD_ITEM, {
+    yield context.df.callEntity(entityId, Operations.ADD_ITEM, {
         itemId: items[2].itemId,
         quantity: 2,
     });
-    yield context.df.callEntity<CartItem>(entityId, Operations.ADD_ITEM, {
+    yield context.df.callEntity(entityId, Operations.ADD_ITEM, {
         itemId: items[1].itemId,
         quantity: 10,
     });
 
-    const cartValue: number = yield context.df.callEntity<number>(entityId, Operations.GET_VALUE);
+    const cartValue: number = yield context.df.callEntity(entityId, Operations.GET_VALUE);
 
     console.log(cartValue);
 });
