bbada165fd612ad32e2f27e7236756d767764f82
hossam-nasr <hossam.nasr99@gmail.com>
2022-03-23 13:11:05 -0700

    remove create long timer action

--- src/actions/createlongtimeraction.ts
+++
@@ -1,6 +0,0 @@
-import { ActionType } from "./actiontype";
-import { IAction } from "./iaction";
-
-export class CreateLongTimerAction implements IAction {
-    public readonly actionType: ActionType;
-}
