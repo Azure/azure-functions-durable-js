3a218009f498007d9b47d9d3f40af0bd17982170
hossam-nasr <hossam.nasr99@gmail.com>
2022-10-06 11:54:46 -0700

    update comments for testing utils for orchestrations

--- src/testingUtils.ts
+++ src/testingUtils.ts
@@ -21,16 +21,9 @@ export class DummyOrchestrationContext extends InvocationContext
      * All parameters are optional but are exposed to enable flexibility
      * in the testing process.
      *
-     * @param instanceId The instanceId of the orchestration
-     * @param history The history events array of the orchestration
-     * @param input The input to the orchestration
-     * @param currentUtcDateTime The deterministic date at the beginning of orchestration replay
-     * @param isReplaying Whether the orchestration is to be marked as isReplaying the its first event
-     * @param longRunningTimerIntervalDuration The duration to break smaller timers into if a long timer exceeds the maximum allowed duration
-     * @param maximumShortTimerDuration The maximum duration for a timer allowed by the underlying storage infrastructure
-     * @param defaultHttpAsyncRequestSleepDurationInMillseconds The default amount of time to wait between sending requests in a callHttp polling scenario
-     * @param schemaVersion The schema version currently used after being negotiated with the extension
-     * @param parentInstanceId The instanceId of the orchestration's parent, if this is a sub-orchestration
+     * @param functionName The name of the orchestration function
+     * @param invocationId The ID of this particular invocation of the orchestration
+     * @param logHandler A handler to emit logs coming from the orchestration function
      */
     constructor(
         functionName = "dummyContextFunctionName",
@@ -52,6 +45,23 @@ export class DummyOrchestrationContext extends InvocationContext
 }
 
 export class DurableOrchestrationInput extends DurableOrchestrationBindingInfo {
+    /**
+     * Creates a new instance of a dummy orchestration input/trigger value,
+     * which is passed to the orchestration on invocation for testing purposes.
+     *
+     * All parameters are optional but are exposed to enable flexibility
+     * in the testing process.
+     *
+     * @param instanceId The instanceId of the orchestration
+     * @param history The history events array of the orchestration
+     * @param input The input to the orchestration
+     * @param longRunningTimerIntervalDuration The duration to break smaller timers into if a long timer exceeds the maximum allowed duration
+     * @param maximumShortTimerDuration The maximum duration for a timer allowed by the underlying storage infrastructure
+     * @param defaultHttpAsyncRequestSleepTimeMillseconds The default amount of time to wait between sending requests in a callHttp polling scenario
+     * @param schemaVersion The schema version currently used after being negotiated with the extension
+     * @param isReplaying Whether the orchestration is to be marked as isReplaying the its first event
+     * @param parentInstanceId The instanceId of the orchestration's parent, if this is a sub-orchestration
+     */
     constructor(
         instanceId = "",
         history: HistoryEvent[] | undefined = undefined,
