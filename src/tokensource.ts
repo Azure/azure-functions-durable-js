6fe50aaf090924da3ce056890326d5e8f661d1eb
hossam-nasr <hossam.nasr99@gmail.com>
2023-01-30 13:36:57 -0800

    update token source

--- src/tokensource.ts
+++ src/tokensource.ts
@@ -1,21 +1,6 @@
-/**
- * Token Source implementation for [Azure Managed Identities](https://docs.microsoft.com/azure/active-directory/managed-identities-azure-resources/overview).
- *
- * @example Get a list of Azure Subscriptions by calling the Azure Resource Manager HTTP API.
- * ```javascript
- * const df = require("durable-functions");
- *
- * module.exports = df.orchestrator(function*(context) {
- *     return yield context.df.callHttp(
- *         "GET",
- *         "https://management.azure.com/subscriptions?api-version=2019-06-01",
- *         undefined,
- *         undefined,
- *         df.ManagedIdentityTokenSource("https://management.core.windows.net"));
- * });
- * ```
- */
-export class ManagedIdentityTokenSource {
+import * as types from "./types";
+
+export class ManagedIdentityTokenSource implements types.ManagedIdentityTokenSource {
     /** @hidden */
     public readonly kind: string = "AzureManagedIdentity";
 
