3794ad6967c47dde8fc67ce3b61287e4409fddb2
Hossam Mabed <31644556+hossam-nasr@users.noreply.github.com>
2023-02-03 11:51:11 -0800

    Add tests again for getClient (#466)
    * add old tests
    
    * update tests
    
    switch to using setInput

--- test/unit/getclient-spec.ts
+++ test/unit/getclient-spec.ts
@@ -1,9 +1,141 @@
+import { InvocationContext } from "@azure/functions";
+import { expect } from "chai";
 import chai = require("chai");
 import chaiAsPromised = require("chai-as-promised");
 import "mocha";
 import "process";
+import { DurableClientInput, getClient, input } from "../../src";
+import {
+    Constants,
+    DurableOrchestrationClient,
+    OrchestrationClientInputData,
+} from "../../src/classes";
+import { TestConstants } from "../testobjects/testconstants";
+import { TestUtils } from "../testobjects/testutils";
 
 chai.use(chaiAsPromised);
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-describe("getClient()", () => {});
+describe("getClient()", () => {
+    const defaultTaskHub = "TestTaskHub";
+    const defaultConnection = "Storage";
+
+    const defaultClientInputOptions: DurableClientInput = input.durableClient();
+
+    const defaultClientInputData: OrchestrationClientInputData = TestUtils.createOrchestrationClientInputData(
+        TestConstants.idPlaceholder,
+        Constants.DefaultLocalOrigin,
+        defaultTaskHub,
+        defaultConnection
+    );
+
+    const v1ClientInputData: OrchestrationClientInputData = TestUtils.createV1OrchestrationClientInputData(
+        TestConstants.idPlaceholder,
+        Constants.DefaultLocalOrigin,
+        defaultTaskHub,
+        defaultConnection
+    );
+
+    const defaultContext: InvocationContext = new InvocationContext({
+        options: {
+            extraInputs: [defaultClientInputOptions],
+        },
+    });
+    defaultContext.extraInputs.set(defaultClientInputOptions, defaultClientInputData);
+
+    const v1Context: InvocationContext = new InvocationContext({
+        options: {
+            extraInputs: [defaultClientInputOptions],
+        },
+    });
+    v1Context.extraInputs.set(defaultClientInputOptions, v1ClientInputData);
+
+    it("throws if context.options is undefined", async () => {
+        expect(() => {
+            const context = new InvocationContext();
+            getClient(context);
+        }).to.throw(
+            "Could not find a registered durable client input binding. Check your extraInputs definition when registering your function."
+        );
+    });
+
+    it("throws if context.options does not contain valid durableClient input binding", async () => {
+        expect(() => {
+            const badInput = { name: "", type: "notDurableInput" };
+            const badContext = new InvocationContext({ options: { extraInputs: [badInput] } });
+            getClient(badContext);
+        }).to.throw(
+            "Could not find a registered durable client input binding. Check your extraInputs definition when registering your function."
+        );
+    });
+
+    it("returns DurableOrchestrationClient if called with valid context", async () => {
+        const client = getClient(defaultContext);
+        expect(client).to.be.instanceOf(DurableOrchestrationClient);
+    });
+
+    it("returns DurableOrchestrationClient if called with V1 context", async () => {
+        const client = getClient(v1Context);
+        expect(client).to.be.instanceOf(DurableOrchestrationClient);
+    });
+
+    describe("Azure/azure-functions-durable-js#28 patch", () => {
+        let WEBSITE_HOSTNAME: string | undefined;
+        beforeEach(() => {
+            WEBSITE_HOSTNAME = process.env.WEBSITE_HOSTNAME;
+        });
+
+        afterEach(() => {
+            process.env.WEBSITE_HOSTNAME = WEBSITE_HOSTNAME;
+        });
+
+        it("corrects API endpoints if WEBSITE_HOSTNAME environment variable not found", async () => {
+            delete process.env.WEBSITE_HOSTNAME;
+
+            const badClientData: OrchestrationClientInputData = TestUtils.createOrchestrationClientInputData(
+                TestConstants.idPlaceholder,
+                "http://0.0.0.0:12345",
+                defaultTaskHub,
+                defaultConnection
+            );
+
+            const clientInput: DurableClientInput = input.durableClient();
+            const badContext: InvocationContext = new InvocationContext({
+                options: {
+                    extraInputs: [clientInput],
+                },
+            });
+            badContext.extraInputs.set(clientInput, badClientData);
+
+            const client = getClient(badContext);
+
+            const expectedUniqueWebhookOrigins: string[] = [Constants.DefaultLocalOrigin];
+            expect(client.uniqueWebhookOrigins).to.deep.equal(expectedUniqueWebhookOrigins);
+        });
+
+        it("corrects API endpoints if WEBSITE_HOSTNAME environment variable is 0.0.0.0", async () => {
+            // Azure Functions Core Tools sets WEBSITE_HOSTNAME to 0.0.0.0:{port} on startup
+            process.env.WEBSITE_HOSTNAME = "0.0.0.0:12345";
+
+            const badClientData: OrchestrationClientInputData = TestUtils.createOrchestrationClientInputData(
+                TestConstants.idPlaceholder,
+                `http://${process.env.WEBSITE_HOSTNAME}`,
+                defaultTaskHub,
+                defaultConnection
+            );
+
+            const clientInput: DurableClientInput = input.durableClient();
+            const badContext: InvocationContext = new InvocationContext({
+                options: {
+                    extraInputs: [clientInput],
+                },
+            });
+            badContext.extraInputs.set(clientInput, badClientData);
+
+            const client = getClient(badContext);
+
+            const expectedUniqueWebhookOrigins: string[] = [Constants.DefaultLocalOrigin];
+            expect(client.uniqueWebhookOrigins).to.deep.equal(expectedUniqueWebhookOrigins);
+        });
+    });
+});
