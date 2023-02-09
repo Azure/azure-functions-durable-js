c9c1e1df6dc636c71d99a275b1378acce4118d18
hossam-nasr <hossam.nasr99@gmail.com>
2023-02-08 17:11:46 -0800

    remove old export

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,6 @@ import { getClient } from "./durableorchestrationclient";
 import { app, input, trigger } from "./shim";
 import { ManagedIdentityTokenSource } from "./tokensource";
 
-export * from "./types";
-
 export {
     DurableHttpRequest,
     DurableHttpResponse,
