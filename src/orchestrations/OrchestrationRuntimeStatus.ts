75d6936996324d33faba7cecb6cf4211c618d0f7
naiyuantian@microsoft.com <naiyuantian@microsoft.com>
2024-01-31 13:19:34 -0800

    replace single quota

--- src/orchestrations/OrchestrationRuntimeStatus.ts
+++ src/orchestrations/OrchestrationRuntimeStatus.ts
@@ -6,5 +6,5 @@ export enum OrchestrationRuntimeStatus {
     Canceled = "Canceled",
     Terminated = "Terminated",
     Pending = "Pending",
-    Suspended = 'Suspended',
+    Suspended = "Suspended",
 }
